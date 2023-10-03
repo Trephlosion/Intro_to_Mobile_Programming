fa960b1074c158d9a782487d25e5797890964558
Tre <rodneywhitneyiii@gmail.com>
2023-10-03 17:00:01 -0400

    Finishing Up

--- CityGuide/app/src/main/java/com/example/cityguide/MainActivity.java
+++ CityGuide/app/src/main/java/com/example/cityguide/MainActivity.java
@@ -1,19 +1,17 @@
 package com.example.cityguide;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ListActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.Objects;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     ListView listView;
 
@@ -22,36 +20,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        Objects.requireNonNull(getActionBar()).setDisplayShowHomeEnabled(true);
+        getActionBar().setLogo(R.mipmap.ic_launcher);
+        getActionBar().setDisplayUseLogoEnabled(true);
 
         String[] attraction = new String[]{"Art Institute of Chicago", "Magnificent Mile", "Willis Tower", "Chicago Bean", "Water Tower"};
         listView = findViewById(R.id.listView);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, attraction);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener());
-
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://artic.edu")));
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-            switch (position){
-                case 0:
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-
-            }
-
-
-        };
-
+        setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, R.id.travel, attraction));
     }
+        protected void onListItemClick(ListView l, View v, int position, long id){
+
+        switch(position){
+            case 0:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://artic.edu")));
+                break;
+            case 1:
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://themagnificentmile.com")));
+                break;
+            case 2:
+                startActivity(new Intent(MainActivity.this, Willis.class));
+                break;
+            case 3:
+                startActivity(new Intent(MainActivity.this, Pier.class));
+                break;
+            case 4:
+                startActivity(new Intent(MainActivity.this, Water.class));
+                break;
+        }
+
+        }
+
+
+
+
+
+
 }
