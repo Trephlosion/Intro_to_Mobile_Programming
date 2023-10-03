8076d9784c7440ad9467c9ed30668a9089e78914
Tre <rodneywhitneyiii@gmail.com>
2023-10-03 17:20:40 -0400

    Finishing Up

--- CityGuide/app/src/main/java/com/example/cityguide/MainActivity.java
+++ CityGuide/app/src/main/java/com/example/cityguide/MainActivity.java
@@ -5,13 +5,16 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Objects;
 
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends AppCompatActivity {
 
     ListView listView;
 
@@ -26,34 +29,35 @@ public class MainActivity extends ListActivity {
 
         String[] attraction = new String[]{"Art Institute of Chicago", "Magnificent Mile", "Willis Tower", "Chicago Bean", "Water Tower"};
         listView = findViewById(R.id.listView);
-        setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, R.id.travel, attraction));
+        ArrayAdapter<String> adapter = (new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                attraction));
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://artic.edu")));
+                        break;
+                    case 1:
+                        startActivity(new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("http://themagnificentmile.com")));
+                        break;
+                    case 2:
+                        startActivity(new Intent(MainActivity.this, Willis.class));
+                        break;
+                    case 3:
+                        startActivity(new Intent(MainActivity.this, Pier.class));
+                        break;
+                    case 4:
+                        startActivity(new Intent(MainActivity.this, Water.class));
+                        break;
+                }
+            }
+        });
+
+
     }
-        protected void onListItemClick(ListView l, View v, int position, long id){
-
-        switch(position){
-            case 0:
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://artic.edu")));
-                break;
-            case 1:
-                startActivity(new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("http://themagnificentmile.com")));
-                break;
-            case 2:
-                startActivity(new Intent(MainActivity.this, Willis.class));
-                break;
-            case 3:
-                startActivity(new Intent(MainActivity.this, Pier.class));
-                break;
-            case 4:
-                startActivity(new Intent(MainActivity.this, Water.class));
-                break;
-        }
-
-        }
-
-
-
-
-
-
 }
